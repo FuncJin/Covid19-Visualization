2acb7ed9f9acdda88f4dec8904d99b9f8d1477c9
Ssylqwj1212 <1767439745@qq.com>
2023-04-05 16:32:31 +0800

    fix: 修复了数据切换时可能造成的动画偏移问题

--- vue-app/src/components/SwitchContent/index.vue
+++ vue-app/src/components/SwitchContent/index.vue
@@ -1,28 +1,32 @@
 <template>
     <div class="switch-content">
-        <div class="s-c-container" :style="{ transform: `translateY(${distance})` }">
+        <div class="s-c-container" :style="{ marginTop: distance }">
             <p v-for="(item, key) of list" :key="key">{{ item }}</p>
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, reactive, watchEffect } from 'vue'
+import { ref, reactive, watch } from 'vue'
 
 type Props = { text: string | number }
 
 const props = defineProps<Props>()
 
+/** 内容列表 */
 const list = reactive([props.text])
+/** 每次移动的距离 */
 const distance = ref('0')
 
-watchEffect(() => {
-    // 追加dom
-    list.push(props.text)
-    const len = list.length - 1
-    if (!len) return
-    distance.value = `-${35 * len}px`
-})
+watch(
+    () => props.text,
+    () => {
+        // 追加dom
+        list.push(props.text)
+        // 更改位置
+        distance.value = `-${35 * (list.length - 1)}px`
+    }
+)
 </script>
 
 <style lang="less" scoped>
@@ -30,7 +34,10 @@ watchEffect(() => {
     height: 100%;
     overflow: hidden;
     .s-c-container {
-        transition: transform 1s;
+        transition: all 1s;
+        p {
+            height: 35px;
+        }
     }
 }
 </style>
